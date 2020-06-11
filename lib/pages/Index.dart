858655338b9b1d9a725cb91609338b07c64e903c
volcan0 <east.qrj@gmail.com>
2020-06-11 22:19:55 +0800

    index

--- lib/pages/Index.dart
+++ lib/pages/Index.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vivo/components/RadiusBox.dart';
 
@@ -17,74 +18,157 @@ class _IndexPage extends State<IndexPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: _customAppBar(),
+        title: renderAppBar(),
       ),
-      body: ListView(children: [
-        SizedBox(height: 20,),
-        Padding(
-          padding: EdgeInsets.only(left: 20, right: 20),
-          child: RadiusBox(child: Image.network("https://p.ssl.qhimg.com/sdm/420_207_/t01bdc902c3571b1ba0.jpg")),
-        ),
-        ListTile(
-          title: Text("国内流量充值"),
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 20, right: 20),
-          child: RadiusBox(child: Image.network("https://p.ssl.qhimg.com/sdm/420_207_/t01bdc902c3571b1ba0.jpg")),
-        ),
-        ListTile(
-            title: Text("境外流量充值"),
-            trailing:Container(
-              width: 90,
-              child: Row(
-                  children: [
-                    Text("查看全部", style: TextStyle(color: Color(0xffcccccc), fontSize: 14),),
-                    SizedBox(width: 4,),
-                    Icon(Icons.chevron_right, color: Color(0xffcccccc))
-                  ]
-              ),
-            )
-        ),
-
-        Padding(
-            padding: EdgeInsets.only(left: 20, right: 20),
-          child: Container(
-            height: 360,
-            child: GridView.count(
-              crossAxisCount: 3,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              children: _gridList(),
-            ),
-          ),
-        )
-
-        
-      ]),
+      body: CustomScrollView(
+        slivers: [
+          renderSwiper(),
+          renderSecondWidget(),
+          renderGridView(),
+          renderScrollView()
+        ],
+      ),
     );
   }
 
-  // gridList
-  List<Widget> _gridList() {
-    var list = new List(9);
-
-    return list.map((e) => _getContainer()).toList();
+  // 第一轮播图
+  Widget renderSwiper() {
+    return SliverToBoxAdapter(
+      child: Container(
+          padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+          child: RadiusBox(
+              child: Image.network(
+            "https://p.ssl.qhimg.com/sdm/420_207_/t01bdc902c3571b1ba0.jpg",
+            fit: BoxFit.cover,
+          ))),
+    );
   }
 
+  // 第二轮播图
+  Widget renderSecondWidget() {
+    return SliverToBoxAdapter(
+      child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 14),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "境外流量充值",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Row(children: [
+                    Text(
+                      "查看全部",
+                      style: TextStyle(color: Color(0xffcccccc), fontSize: 14),
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Icon(Icons.chevron_right, color: Color(0xffcccccc))
+                  ])
+                ],
+              ),
+            ),
+            RadiusBox(
+                child: Image.network(
+                    "https://p.ssl.qhimg.com/sdm/420_207_/t01bdc902c3571b1ba0.jpg")),
+          ])),
+    );
+  }
+
+  // 境外流量充值
+  Widget renderGridView() {
+    var list = new List(9);
+    return SliverToBoxAdapter(
+        child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 14),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "境外流量充值",
+                  style: TextStyle(fontSize: 16),
+                ),
+                Row(children: [
+                  Text(
+                    "查看全部",
+                    style: TextStyle(color: Color(0xffcccccc), fontSize: 14),
+                  ),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Icon(Icons.chevron_right, color: Color(0xffcccccc))
+                ])
+              ],
+            ),
+          ),
+          GridView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: list.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                childAspectRatio: 1.3,
+              ),
+              itemBuilder: (context, index) {
+                return _getContainer();
+              })
+        ],
+      ),
+    ));
+  }
+
+  // grid子项
   Widget _getContainer() {
     return Container(
-      alignment: Alignment.center,
-      height: 100,
-      child: Text(
-        "ddd",
-        style: TextStyle(color: Colors.white, fontSize: 20),
+//    color: Colors.green,
+      child: RadiusBox(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.maxFinite,
+              height: 60,
+              child: Image.network(
+                "https://img2.woyaogexing.com/2020/06/10/ceed483c1df945b7bd01e0a22c6bf96b!400x400.jpeg",
+                fit: BoxFit.fill,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  left: BorderSide(width: 1, color: Color(0xffdddddd)),
+                  bottom: BorderSide(width: 1, color: Color(0xffdddddd)),
+                  right: BorderSide(width: 1, color: Color(0xffdddddd)),
+                ),
+              ),
+              alignment: Alignment.center,
+              child: Padding(
+                  padding: EdgeInsets.only(
+                    top: 6,
+                    bottom: 6,
+                  ),
+                  child: Text(
+                    "4元起",
+                    style: TextStyle(color: Colors.orangeAccent, fontSize: 12),
+                  )),
+            )
+          ],
+        ),
       ),
-      color: Colors.blue,
     );
   }
 
   // 自定义头部
-  Widget _customAppBar() {
+  Widget renderAppBar() {
     return Container(
       width: 400,
       decoration: BoxDecoration(
@@ -107,5 +191,77 @@ class _IndexPage extends State<IndexPage> {
         ],
       ),
     );
+  }
+
+  // 滚动视图
+  Widget renderScrollView() {
+    return SliverToBoxAdapter(
+        child: Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Column(children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "境外跨地区通用",
+                style: TextStyle(fontSize: 16),
+              ),
+              Row(children: [
+                Text(
+                  "查看全部",
+                  style: TextStyle(color: Color(0xffcccccc), fontSize: 14),
+                ),
+                SizedBox(
+                  width: 4,
+                ),
+                Icon(Icons.chevron_right, color: Color(0xffcccccc))
+              ])
+            ],
+          ),
+        ),
+        Container(
+          height: 100,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 10,
+            itemExtent: 150,
+            itemBuilder: (BuildContext context, int idx) {
+              return Padding(
+                padding: EdgeInsets.all(5),
+                child: RadiusBox(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Container(
+                        width: double.maxFinite,
+                        height: 60,
+                        child: Image.network("https://img2.woyaogexing.com/2020/06/11/1cc80fb092ec4fa1a176ce626e11b0f8!400x400.jpeg", fit: BoxFit.cover,),
+                      ),
+                      Container(
+                        width: double.maxFinite,
+                        height: 30,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          border: new Border.all(width: 1, color: Colors.black),
+//                          border: Border(
+//                            left: BorderSide(width: 1, color: Colors.black),
+//                            bottom: BorderSide(width: 1, color: Colors.black),
+//                            right: BorderSide(width: 1, color: Colors.black),
+//                          ),
+                          borderRadius: BorderRadius.only(bottomRight: Radius.circular(4),bottomLeft: Radius.circular(4))
+                        ),
+                        child: Text("dsdsds"),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        )
+      ]),
+    ));
   }
 }
